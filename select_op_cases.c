fd74d55e4f313072e0a963b76252a857bf4f5be7
3387-hub <3387@holbertonschool.com>
2022-01-12 18:24:42 -0500

    I add the function that select the functions in case of match.

---
+++ select_op_cases.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ *select_opcodes - Function that copares the op codes with an array
+ *that contains function to execute in case of match with op_codes.
+ *@token: op_case tokenize which we´ll compare to select function.
+ *Return: in case of match return the function else: return NULL.
+ */
+
+void (*select_opcodes(char *token))(stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+
+	instruction_t op_codes[] = {
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}};
+
+	while(op_codes[i].opcode != NULL)
+	{
+		if (strcmp(op_codes[i].opcode, token) == 0)
+		{
+			return (op_codes[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
